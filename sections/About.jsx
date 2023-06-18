3ccd8771a0f3cabca6754e6cf936751ee484121a
Bhavik Patel <bhavikpatel@Bhaviks-MacBook-Air.local>
2023-06-17 21:09:30 -0400

    first commit

---
+++ sections/About.jsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Me |" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">Bhavik</span> is a new
+        graduate from Rutgers University,
+        currently based in New Jersey and is always keen to learn technologies,
+        frameworks, programming languages along with being passionate about is passionate{' '}
+        <span className="font-extrabold text-white">
+          creating and developing new ideas
+        </span>{' '}
+        of today. Let's{' '}
+        <span className="font-extrabold text-white">explore</span> more by scrolling
+        down
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
