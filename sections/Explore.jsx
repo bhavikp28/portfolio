f20f0687895d0ed24599e4e38895b2191e718b7d
Bhavik Patel <bhavikpatel@Bhaviks-MacBook-Air.local>
2023-06-25 01:25:01 -0400

    added deployment startup time warning

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -25,7 +25,7 @@ const Explore = () => {
           title={<>Choose the project you wish <br className="md:block hidden" /> to explore</>}
           textStyles="text-center"
         />
-        <TypingText title="Click the github icon to see source code" textStyles="text-center" />
+        <TypingText title="Click the github icon to see source code & Please allow time for deployment to fully start up" textStyles="text-center" />
         <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
           {exploreWorlds.map((world, index) => (
             <ExploreCard
